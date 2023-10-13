399a7e53162097ba7ba7040f2c94cc505a7848b8
Yoru-Cyber <carlosmendez170210@gmail.com>
2023-10-13 00:51:22 -0400

    ciclo for para eliminar todas las apariciones de la columna Acciones con sus botones

--- App/static/exportExcel.js
+++ App/static/exportExcel.js
@@ -1,8 +1,11 @@
 function exportExcel() {
   
   let sheet = XLSX.utils.table_to_sheet(document.getElementById("TableProducts"));
-  delete(sheet['G1']);
-  delete(sheet['G2']);
+  let range = XLSX.utils.decode_range(sheet['!fullref']);
+  const { r } = range.e;
+  for (let i = 0; i <= r; i++) {
+    delete(sheet[`G${i + 1}`]);
+  }
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, sheet, "Ventas");
   XLSX.writeFile(workbook, `Ventas ${new Date()}.xlsx`);
